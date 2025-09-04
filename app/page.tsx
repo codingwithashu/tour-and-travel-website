7a2495050f7a18da0dbc6e7394a1afa56634542e
Ashutosh Gupta <ashutosh.entrepreneur@gmail.com>
2025-09-04 17:52:01 +0530

    rm 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,20 @@
-import { HeroSection } from "@/components/hero-section";
-import { FeaturedPackages } from "@/components/featured-packages";
-import { WhyChooseUs } from "@/components/why-choose-us";
 import { Navigation } from "@/components/navigation";
 import { Footer } from "@/components/footer";
 import { PageTransition } from "@/components/page-transition";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { getQueryClient, trpc } from "@/trpc/server";
 
-export const dymanic = "force-dynamic";
 
 export default async function Page() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.packages.getAll.queryOptions());
-
   return (
     <PageTransition>
       <div className="min-h-screen bg-background">
         <Navigation />
-        <main>
+        {/* <main>
           <HeroSection />
           <HydrationBoundary state={dehydrate(queryClient)}>
             <FeaturedPackages />
           </HydrationBoundary>
           <WhyChooseUs />
-        </main>
+        </main> */}
         <Footer />
       </div>
     </PageTransition>
